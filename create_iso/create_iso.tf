bc4ac8ef3da644832dd48e9b031d3627a87e3dfa
Ming Hou <ming@ddtechcg.com>
2020-12-13 12:16:26 -0500

    refresh

--- create_iso/create_iso.tf
+++ create_iso/create_iso.tf
@@ -66,6 +66,7 @@ resource "null_resource" "downloadiso" {
     inline = [
       "yum install -y wget",
       "yum install -y mkisofs",
+      "yum install -y telnet",
       "curl -sL -o /tmp/installer.iso ${var.binaries["openshift_iso"]}",
       "test -e /tmp/tempiso || mkdir /tmp/tempiso",
       "sudo mount /tmp/installer.iso /tmp/tempiso",
