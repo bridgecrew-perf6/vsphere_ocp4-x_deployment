2e71094b73536410e637fd8feefe76f32838ec0f
Ming Hou <ming@ddtechcg.com>
2020-12-13 22:57:55 -0500

    tested workers

--- create_iso/create_iso.tf
+++ create_iso/create_iso.tf
@@ -5,9 +5,9 @@ resource "null_resource" "dependency" {
 }
 
 locals {
-  #workers = {[
-  #  for i in range(${length(var.worker_ips)}) : format("worker%d", i)
-  #]}
+  t_workers = [
+    for i in range(${length(var.worker_ips)}) : format("worker%d", i)
+  ]
 
   all_hostnames = concat(list(var.bootstrap), var.masters, var.workers)
   all_ips       = concat(list(var.bootstrap_ip), var.master_ips, var.worker_ips)
