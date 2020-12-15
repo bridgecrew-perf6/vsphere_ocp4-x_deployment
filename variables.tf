3a85d9cc9f1626af44d425b79978e42ad1892af1
Ming Hou <ming@ddtechcg.com>
2020-12-15 10:43:57 -0500

    rolled back

--- variables.tf
+++ variables.tf
@@ -134,3 +134,15 @@ variable "vsphere_image_datastore_path" {
 variable "no_proxies" {
   type = list(string)
 }
+
+variable "binaries" {
+  type = map(string)
+  default = {
+    openshift_iso       = "https://mirror.openshift.com/pub/openshift-v4/x86_64/dependencies/rhcos/4.5/4.5.6/rhcos-4.5.6-x86_64-installer.x86_64.iso"
+    openshift_bios      = "https://mirror.openshift.com/pub/openshift-v4/x86_64/dependencies/rhcos/4.5/4.5.6/rhcos-4.5.6-x86_64-metal.x86_64.raw.gz"
+    openshift_kernel    = "https://mirror.openshift.com/pub/openshift-v4/x86_64/dependencies/rhcos/4.5/4.5.6/rhcos-4.5.6-x86_64-installer-kernel-x86_64"
+    openshift_initramfs = "https://mirror.openshift.com/pub/openshift-v4/x86_64/dependencies/rhcos/4.5/4.5.6/rhcos-4.5.6-x86_64-installer-initramfs.x86_64.img"
+    govc                = "https://github.com/vmware/govmomi/releases/download/v0.22.1/govc_linux_amd64.gz"
+  }
+}
+
